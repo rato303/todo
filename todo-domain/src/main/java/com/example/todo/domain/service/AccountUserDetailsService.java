980db3af54e5059e0ef3c68606b7a13d8fc610ab
Toshiya Kimura <toshiya_kimura@bs-ja.co.jp>
2022-01-01 17:41:46 +0900

    画面からAccountUserDetailsServiceが呼び出されるように実装

---
+++ todo-domain/src/main/java/com/example/todo/domain/service/AccountUserDetailsService.java
@@ -0,0 +1,81 @@
+package com.example.todo.domain.service;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.todo.domain.model.Account;
+import com.example.todo.domain.repository.account.AccountRepository;
+
+@Service
+@Transactional
+public class AccountUserDetailsService implements UserDetailsService {
+	
+	AccountRepository accountRepository;
+	
+	AccountUserDetailsService(AccountRepository accountRepository) {
+		this.accountRepository = accountRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Account account = accountRepository.findByName(username);
+		return new UserDetailsImpl(account);
+	}
+	
+	private class UserDetailsImpl implements UserDetails {
+
+		private static final long serialVersionUID = 1095316793216814073L;
+		
+		Account account;
+		
+		UserDetailsImpl(Account account) {
+			this.account = account;
+		}
+
+		@Override
+		public Collection<? extends GrantedAuthority> getAuthorities() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public String getPassword() {
+			return account.getPassword();
+		}
+
+		@Override
+		public String getUsername() {
+			return account.getName();
+		}
+
+		@Override
+		public boolean isAccountNonExpired() {
+			// always
+			return true;
+		}
+
+		@Override
+		public boolean isAccountNonLocked() {
+			// always
+			return true;
+		}
+
+		@Override
+		public boolean isCredentialsNonExpired() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean isEnabled() {
+			// always
+			return true;
+		}
+		
+	}
+
+}
