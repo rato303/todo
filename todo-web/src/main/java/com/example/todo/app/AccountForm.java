7a233b0623db7573e4dbacdb9eace463175bf40b
Toshiya Kimura <toshiya_kimura@bs-ja.co.jp>
2022-01-02 18:46:58 +0900

    画面から権限を受け取れるように変更

--- todo-web/src/main/java/com/example/todo/app/AccountForm.java
+++ todo-web/src/main/java/com/example/todo/app/AccountForm.java
@@ -1,7 +1,11 @@
 package com.example.todo.app;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import com.example.todo.domain.model.Authority;
 import com.example.todo.domain.service.AccountCreateService.AccountForCreate;
 
 public class AccountForm {
@@ -10,6 +14,8 @@ public class AccountForm {
 	
 	private String password;
 	
+	private String authorities;
+
 	public AccountForm() {
 		this.name = "";
 		this.password = "";
@@ -34,18 +40,37 @@ public class AccountForm {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	@Override
-	public String toString() {
-		return "AccountForm [name=" + name + ", password=" + password + "]";
+	
+	public String getAuthorities() {
+		return authorities;
 	}
 	
+	public void setAuthorities(String authorities) {
+		this.authorities = authorities;
+	}
+	
+	public List<Authority> asAuthorities() {
+		String[] split = this.authorities.split(",");
+		return Arrays.stream(split)
+				.map((v) -> {
+					return Authority.stringOf(v);
+				})
+				.collect(Collectors.toList());
+	}
+	
+	@Override
+	public String toString() {
+		return "AccountForm [name=" + name + ", password=" + password + ", authorities=" + authorities + "]";
+	}
+
 	private class AccountForCreateImpl implements AccountForCreate {
 		
 		AccountForm form;
+		List<Authority> authorities;
 		
 		AccountForCreateImpl(AccountForm form) {
 			this.form = form;
+			this.authorities = form.asAuthorities();
 		}
 
 		@Override
@@ -62,6 +87,11 @@ public class AccountForm {
 		@Override
 		public String getRawPassword() {
 			return form.getPassword();
+		}
+
+		@Override
+		public List<Authority> getAuthorities() {
+			return authorities;
 		}
 		
 	}
