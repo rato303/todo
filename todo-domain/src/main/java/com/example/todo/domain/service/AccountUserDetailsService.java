e3be9f4cd54ec9128f5ea0666aed1dc1e3b17042
Toshiya Kimura <toshiya_kimura@bs-ja.co.jp>
2022-01-02 19:39:15 +0900

    表示できるページを権限によって制御するように実装

--- todo-domain/src/main/java/com/example/todo/domain/service/AccountUserDetailsService.java
+++ todo-domain/src/main/java/com/example/todo/domain/service/AccountUserDetailsService.java
@@ -1,5 +1,12 @@
 package com.example.todo.domain.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,22 +15,30 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.example.todo.domain.model.Account;
 import com.example.todo.domain.model.AccountUserDetails;
+import com.example.todo.domain.model.AuthorityImpl;
 import com.example.todo.domain.repository.account.AccountRepository;
+import com.example.todo.domain.repository.authority.AuthorityRepository;
 
 @Service
 @Transactional
 public class AccountUserDetailsService implements UserDetailsService {
 
 	AccountRepository accountRepository;
+	AuthorityRepository authorityRepository;
 
-	AccountUserDetailsService(AccountRepository accountRepository) {
+	@Inject
+	AccountUserDetailsService(AccountRepository accountRepository, AuthorityRepository authorityRepository) {
 		this.accountRepository = accountRepository;
+		this.authorityRepository = authorityRepository;
 	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Account account = accountRepository.findByName(username);
-		return new AccountUserDetails(account);
+		List<AuthorityImpl> authorities = authorityRepository.findByAccountId(account.getId());
+		List<String> stringAauthorities = authorities.stream().map(AuthorityImpl::getName).collect(Collectors.toList());
+		List<GrantedAuthority> granteAauthorities = AuthorityUtils.createAuthorityList(stringAauthorities.toArray(new String[stringAauthorities.size()]));
+		return new AccountUserDetails(account, granteAauthorities);
 	}
 
 }
