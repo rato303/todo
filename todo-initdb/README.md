1712c83f845f1851e78fb6dad5ba685bfdec1e3c
Toshiya Kimura <toshiya_kimura@bs-ja.co.jp>
2022-01-02 14:31:18 +0900

    アカウントの初期データを登録するように変更

---
+++ todo-initdb/README.md
@@ -0,0 +1,9 @@
+# 初期データ
+
+- アカウント
+    - account_id:user1
+    - password:user1
+    - account_id:user2
+    - password:user2
+    - account_id:user3
+    - password:user3
---
+++ todo-initdb/src/main/sqls/11_insert_account.sql
@@ -0,0 +1,4 @@
+insert into public.account(account_id,account_name,password) values 
+   ('b3bfc584-9ca0-4aa7-b2e2-b1ab2c1edc45','user1','{pbkdf2}9f975d74a23f578dd790c2e52007b159a47266af7a3f2bdd07719b9e433dcbf8312ed887ea9c94be')
+  , ('7188f1c7-63ae-4376-a1c4-262c402805dd','user2','{pbkdf2}9d7b93c53c32bce74b5bb21ce661ca99f5cfc755e63c477c74c55d9b443c60e224302b868037af83')
+  , ('b5073c91-5e59-4544-b300-9a09c2f31b80','user3','{pbkdf2}9368a7eaa3bc7c249e90ce27557c3ad2ba42bcd8824efd7d6fa4955060180d1e9f6d666be36027e7');
