2d06d7c27c25a9d3b1a55b7972b0976def7c23e7
Toshiya Kimura <toshiya_kimura@bs-ja.co.jp>
2022-01-02 16:12:08 +0900

    認証時にセッションに保持されるクラスをpublicに変更

--- todo-domain/src/main/java/com/example/todo/domain/service/AccountUserDetailsService.java
+++ todo-domain/src/main/java/com/example/todo/domain/service/AccountUserDetailsService.java
@@ -1,9 +1,5 @@
 package com.example.todo.domain.service;
 
-import java.util.Collection;
-import java.util.Collections;
-
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.todo.domain.model.Account;
+import com.example.todo.domain.model.AccountUserDetails;
 import com.example.todo.domain.repository.account.AccountRepository;
 
 @Service
@@ -26,58 +23,7 @@ public class AccountUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Account account = accountRepository.findByName(username);
-		return new UserDetailsImpl(account);
-	}
-
-	private class UserDetailsImpl implements UserDetails {
-
-		private static final long serialVersionUID = 1095316793216814073L;
-
-		Account account;
-
-		UserDetailsImpl(Account account) {
-			this.account = account;
-		}
-
-		@Override
-		public Collection<? extends GrantedAuthority> getAuthorities() {
-			return Collections.emptyList();
-		}
-
-		@Override
-		public String getPassword() {
-			return account.getPassword();
-		}
-
-		@Override
-		public String getUsername() {
-			return account.getName();
-		}
-
-		@Override
-		public boolean isAccountNonExpired() {
-			// always
-			return true;
-		}
-
-		@Override
-		public boolean isAccountNonLocked() {
-			// always
-			return true;
-		}
-
-		@Override
-		public boolean isCredentialsNonExpired() {
-			// always
-			return true;
-		}
-
-		@Override
-		public boolean isEnabled() {
-			// always
-			return true;
-		}
-
+		return new AccountUserDetails(account);
 	}
 
 }
