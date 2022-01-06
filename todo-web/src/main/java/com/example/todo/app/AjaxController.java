097028b94ecbea4812dbcd3d8d10597e62d4435f
Toshiya Kimura <toshiya_kimura@bs-ja.co.jp>
2022-01-06 20:04:10 +0900

    サンプルと同じにしても動作しない？ #18

--- todo-web/src/main/java/com/example/todo/app/AjaxController.java
+++ todo-web/src/main/java/com/example/todo/app/AjaxController.java
@@ -4,25 +4,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Optional;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+@RestController
 public class AjaxController {
 
-	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	@ResponseBody
-	public ResponseEntity<byte[]> upload(@RequestPart("uploadFile") final MultipartFile uploadFile) {
+	@RequestMapping(value = "/moge", method = RequestMethod.POST)
+	public int upload(@ModelAttribute FileUploadForm form) {
+		MultipartFile uploadFile = form.getFile();
 		if (uploadFile.isEmpty()) {
-			return ResponseEntity.of(Optional.empty());
+//			return ResponseEntity.of(Optional.empty());
+			return 0;
 		}
 		final Path path = Paths.get("C:\\home\\developer\\workspace\\", uploadFile.getOriginalFilename());
 		final byte[] bytes;
@@ -30,16 +28,17 @@ public class AjaxController {
 			bytes = uploadFile.getBytes();
 			Files.write(path, bytes);
 		} catch (IOException e) {
-			return ResponseEntity.of(Optional.empty());
+//			return ResponseEntity.of(Optional.empty());
+			return 0;
 		}
-		return ResponseEntity.ok(bytes);
+//		return ResponseEntity.ok(bytes);
+		return 0;
 	}
-	
+
 	@RequestMapping(value = "/hoge", method = RequestMethod.GET)
-	@ResponseBody
 	public String[] execute(Model model) {
-		String[] datas = {"test1", "test2", "test3"};
+		String[] datas = { "test1", "test2", "test3" };
 		return datas;
 	}
-	
+
 }
